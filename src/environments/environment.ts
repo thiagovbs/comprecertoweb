07f9dcb059769eaca2cc8fbbe1da0423bfd73c35
Thiago Veloso <thiago.vbs@gmail.com>
2019-05-31 15:44:46 -0300

    correção do endereço da api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  urlSpring: 'https://sheap.herokuapp.com/rest',
-  urlAuth: 'https://sheap.herokuapp.com',
+  urlSpring: 'https://http://167.86.118.44:4200/rest',
+  urlAuth: 'https://http://167.86.118.44:4200',
   //urlAuth:'http://localhost:8080',
   //urlSpring: 'http://localhost:8080',
   urlS3: 'https://s3-sa-east-1.amazonaws.com/sheap-bucket'
