e22612795e0270988636bdc045660a26a2ea66c5
Vinicius <vinicius.valenca@outlook.com.br>
2019-08-04 17:53:51 -0300

    Pequenos Ajustes v3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
     //urlSpring: 'https://sheap.herokuapp.com/rest',
-  urlSpring: 'http://167.86.118.44:8080/rest',
-  urlAuth: 'http://167.86.118.44:8080',
-  //urlAuth: 'http://localhost:8080',
- //urlSpring: 'http://localhost:8080/rest',
+    //urlAuth: 'https://sheap.herokuapp.com',
+  //urlSpring: 'http://167.86.118.44:8080/rest',
+  //urlAuth: 'http://167.86.118.44:8080',
+  urlAuth: 'http://localhost:8080',
+ urlSpring: 'http://localhost:8080/rest',
   
-  //urlAuth: 'https://sheap.herokuapp.com',
+  
   
   urlS3: 'https://s3-sa-east-1.amazonaws.com/sheap-bucket'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,12 @@
 
 export const environment = {
   production: true,
-  //urlSpring: 'https://sheap.herokuapp.com/rest',
-  urlSpring: 'http://167.86.118.44:8080/rest',
-  urlAuth: 'http://167.86.118.44:8080',
-  //urlAuth: 'http://localhost:8080',
-  //urlSpring: 'http://localhost:8080/rest',
-  
-  //urlAuth: 'https://sheap.herokuapp.com',
+   //urlSpring: 'https://sheap.herokuapp.com/rest',
+    //urlAuth: 'https://sheap.herokuapp.com',
+  //urlSpring: 'http://167.86.118.44:8080/rest',
+  //urlAuth: 'http://167.86.118.44:8080',
+  urlAuth: 'http://localhost:8080',
+ urlSpring: 'http://localhost:8080/rest',
   
   urlS3: 'https://s3-sa-east-1.amazonaws.com/sheap-bucket'
 };
